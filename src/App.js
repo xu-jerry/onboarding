5258f34a9eab69f4a8f39dd09ab950afab0176f8
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2021-11-24 21:18:10 -0800

    Milestone 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Post from './Post';
+import PostsDisplay from './PostDisplay';
 import './App.css';
-
-// airtable configuration
-// const Airtable = require('airtable');
-
-// const airtableConfig = {
-//   apiKey: process.env.REACT_APP_AIRTABLE_USER_KEY,
-//   baseKey: process.env.REACT_APP_AIRTABLE_BASE_KEY,
-// };
-// const base = new Airtable({ apiKey: airtableConfig.apiKey })
-//   .base(airtableConfig.baseKey);
 
 const App = function () {
   return (
-    <>
-      <Post author="Farmer Bob" body="Farmers only" />
-      <Post author="Blue" body="Print" />
-      <Post author="Printer" body="Not twitter I swear" />
-    </>
+    <PostsDisplay />
   );
 };
 
